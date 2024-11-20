6b417213eeb4459c15c67f466e1b346dd58c5ef5
Taruna Wahyudi <wahyuditaruna97@gmail.com>
2024-11-20 23:21:51 +0700

    add Google Analytics and sitemap

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -1,10 +1,11 @@
+'use client'
+
+import {GoogleAnalytics} from "nextjs-google-analytics";
+
 export default function Head() {
   return (
     <>
-      <title> Opencam | Free Conference App</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Conference app totally free" />
-      <link rel="icon" href="/images/favicon/favicon.ico" />
+     <GoogleAnalytics trackPageViews />
     </>
   );
 }
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,18 +2,26 @@ import { Metadata } from "next";
 import Hero from "@/components/Hero";
 import Feature from "@/components/Features";
 import FunFact from "@/components/FunFact";
+import Head from "@/app/(site)/head";
 
 export const metadata: Metadata = {
   title: "Opencam",
-  description: "Free Conference App",
+  description: "Opencam | Free Conference App",
   icons: {
       icon: '/images/favicon/favicon.ico',
-  }
+  },
+  authors: [
+    {
+      name: "Taruna Wahyudi",
+      url: "https://tarunawahyudi.github.io"
+    }
+  ]
 };
 
 export default function Home() {
   return (
     <main>
+      <Head />
       <Hero />
       <Feature />
       <FunFact />
